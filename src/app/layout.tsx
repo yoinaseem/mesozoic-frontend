c44d195b4c7e943305911414d08e87b99b6c1340
Yoi <yoi.naseem@gmail.com>
2026-04-05 21:47:08 +0500

    feat(ui): add shadcn base components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/context/auth-context";
 import "./globals.css";
