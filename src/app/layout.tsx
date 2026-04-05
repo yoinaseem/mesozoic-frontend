85d2ae9f02296ad5f36a515aae6f15605cdc3fd1
Zahwan69 <zahuwansul@gmail.com>
2026-04-05 21:24:04 +0500

    chore: Cleanup Hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-base min-h-screen flex flex-col text-base-color`}
       >
         <Navbar />
-        <main className="flex-1 w-full">{children}</main>
+        <main className="flex-1 w-full pt-16">{children}</main>
         <Footer />
       </body>
     </html>
